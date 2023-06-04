511490447c6fd49647ffcc115146327951ca0405
Rayhan Shorker <raihanshorker56@gmail.com>
2023-06-04 19:51:04 +0600

    create api for add task &get all task

--- index.js
+++ index.js
@@ -28,6 +28,25 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const taskCollection = client.db("task-management").collection("task");
+
+app.get("/task",async(req,res)=>{
+    const cursor = taskCollection.find();
+    const result = await cursor.toArray()
+    res.send(result)
+})
+
+
+
+    app.post("/post-task", async (req, res) => {
+        const body = req.body;
+        console.log(body);
+        const result = await taskCollection.insertOne(body);
+        res.send(result);
+      });
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
